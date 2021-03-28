5437a157ff2f89a75f55356cb58a9aa25e2e12e4
jimmy1141 <50308306+jimmy1141@users.noreply.github.com>
2021-03-29 00:28:08 +0800

    Update xiaoleCOOKIE.js

--- Task/xiaoleCOOKIE.js
+++ Task/xiaoleCOOKIE.js
@@ -1,8 +1,8 @@
 
 
 //独立COOKIE文件     ck在``里面填写，多账号换行
-let xiaoleurlVal= ``
-let xiaoleheaderVal= ``
+let xiaoleurlVal= `https://minapp.xqrobot.net/user.php?mod=index&client=minapp&version=2.7&site=yyq&user_openid=ofGEO5Flj9YhzGLTzk4O4FUsTx8A&user_lbs=&city=&u=11084 `
+let xiaoleheaderVal= `{"Accept-Encoding":"gzip,compress,br,deflate","Cookie":"PHPSESSID=t04bfj9oen8rcjh5ktee925cq1","Connection":"keep-alive","Referer":"https://servicewechat.com/wx7d27c26991711f37/127/page-frame.html","Content-Type":"application/json","Host":"minapp.xqrobot.net","User-Agent":"Mozilla/5.0 (iPhone; CPU iPhone OS 14_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.2(0x18000232) NetType/3G Language/zh_CN"} `
 
 
 
